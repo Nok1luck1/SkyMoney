c91ece2970ba0d4e9b5448aa7b6bd05b4fbbc3e9
Nikolaj <kolambo4ka@gmail.com>
2022-09-07 09:47:23 +0300

    test buying level

--- test/express.js
+++ test/express.js
@@ -1,35 +1,36 @@
 const { ethers } = require("hardhat")
 const { expect, assert } = require("chai")
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+require("hardhat-gas-reporter");
 
 // describe(string, anonymous function)
-describe("SimpleStorage", function () {
-   
-    let SkyMoney, SkyMoneyInstance
+describe("SkyMoney", function () {
+     async function deploySkyMoneyFixture() {
+        const [owner] = await ethers.getSigners();
+        const token = await ethers.getContractFactory("Token");
+        const Token = await token.deploy();
+        await Token.deployed();
+        const skyMoney = await ethers.getContractFactory("SkyMoney")
+        const SkyMoney = await skyMoney.deploy(Token.address,owner.address);
+        await SkyMoney.deployed();
+        console.log(SkyMoney.getLevel);
+        return {SkyMoney,owner,Token}
+    }
 
-    beforeEach(async function () {
-        SkyMoney = await ethers.getContractFactory("SkyMoney")
-        SkyMoneyInstance = await SkyMoney.deploy("0x3f4F5d9971c265a7485540207023EA4B68Af6dc6","0x3f4F5d9971c265a7485540207023EA4B68Af6dc6")
+    it("Should be equal owner  address", async function () {
+        const {SkyMoney,owner} = await loadFixture(deploySkyMoneyFixture);
+        expect(await SkyMoney.owner()).to.equal(owner.address);
     })
-
-    // what to test
-    it("Buy Level instruction", async function () {
-        const currentValue = await SkyMoney.buyLevel(0,"0x3f4F5d9971c265a7485540207023EA4B68Af6dc6");
-        const currentValue1 = await SkyMoney.buyLevel(0,"0x3f4F5d9971c265a7485540207023EA4B68Af6dc6");
-        const currentValue2 = await SkyMoney.buyLevel(0,"0x3f4F5d9971c265a7485540207023EA4B68Af6dc6");
-        const currentValue3 = await SkyMoney.buyLevel(0,"0x3f4F5d9971c265a7485540207023EA4B68Af6dc6");
-        const currentValue4 = await SkyMoney.buyLevel(0,"0x3f4F5d9971c265a7485540207023EA4B68Af6dc6");
-        const currentValue5 = await SkyMoney.buyLevel(0,"0x3f4F5d9971c265a7485540207023EA4B68Af6dc6");
-
-        console.log(currentValue5);
+    it("Should be equal token  address", async function () {
+        const {SkyMoney,Token} = await loadFixture(deploySkyMoneyFixture);
+        expect(await SkyMoney.Token()).to.equal(Token.address);
     })
-
+    it("Should be error on Buy level", async function () {
+        const {SkyMoney,Token,owner} = await loadFixture(deploySkyMoneyFixture);
+        await Token.approve(SkyMoney.address,"400000000000000000000");
+        await SkyMoney.buyLevel("0",owner.address);
+        await SkyMoney.buyLevel("0",owner.address);
+        await expect(SkyMoney.buyLevel("0",owner.address)).to.be.revertedWith("You cant buy more this level yet");
+    })
     
-
-    it("Should update people array when people push is called", async function () {
-        const expectedToken = "0x3f4F5d9971c265a7485540207023EA4B68Af6dc6"
-        const transactionResponse = await SkyMoney.changeToken(expectedToken);
-        const updatedToken = await SkyMoney.Token();
-        assert.equal(expectedToken, updatedToken);
-
-    })
 })
